14663b7e631d87bed35f06ec25f2df24ba1a806f
hyunchang <hyunchang1234@hanmail.net>
2017-05-18 17:36:29 +0900

    Step11_InnerClass

---
+++ src/test/main/MainClass01.java
@@ -0,0 +1,19 @@
+package test.main;
+
+import test.mypac.Bakery;
+// import 된 경로를 참고해 보세요.
+import test.mypac.Bakery.Bread;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		new Bakery("종로지점").getBread().eat();
+		
+		// 위의 작업을 세줄로 표현하면
+		Bakery bakery = new Bakery("을지로지점");
+		Bread bread = bakery.getBread();
+		bread.eat();
+		
+		new Bakery("시청지점").eatCola();
+		
+	}
+}
---
+++ src/test/main/MainClass02.java
@@ -0,0 +1,13 @@
+package test.main;
+
+import test.mypac.CarUser;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		new CarUser().car.drive();
+		
+		new CarUser().useCar();
+		
+		new CarUser().useCar2();
+	}
+}
